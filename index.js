5bcede88be0f214d347342ea6fa6d91ae47770a6
edwi-11 <calero06edwin11@gmail.com>
2026-05-09 22:26:21 -0600

    Funciones nuevas

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
+let editingTaskId = null;
+
 function handleFormSubmit(event) {
 	event.preventDefault();
 
 	const formData = new FormData(event.target);
 	const task = Object.fromEntries(formData);
-	task.id = Date.now();
 	
-	const taskElement = createTaskElement(task);
 	const ulContainer = document.getElementById("task-list-container");
-	if (!ulContainer) return;
+
+	if(editingTaskId){
+		const existingTask = document.getElementById(editingTaskId);
+
+		existingTask.querySelector("h3").textContent = task.title;
+		existingTask.querySelector("p").textContent = task.description;
+
+		alert("La tarea se actualizó correctamente");
+
+		editingTaskId = null;
+
+		document.getElementById("NuevaTarea").textContent = "Nueva Tarea";
+
+        document.querySelector("#agregar button").innerHTML ='<i class="fa-solid fa-plus"></i> Agregar Tarea';
+
+		document.getElementById("cancelar").style = "none";
+
+		event.target.reset();
+
+		return;
+	}
+
+
+	task.id = Date.now();
+
+	const taskElement = createTaskElement(task);
+
+	ulContainer.appendChild(taskElement);
+
+	event.target.reset();
 
 	ulContainer.appendChild(taskElement);
 	// ulContainer.innerHTML = taskElement;
@@ -30,7 +59,7 @@ function createTaskElement (task) {
 	const deleteButton = document.createElement("button");
 	deleteButton.textContent = "Eliminar";
 	deleteButton.style.background = "linear-gradient(135deg, #ff6b6b, #ff8e8e)";
-	deleteButton.style.borderRadius = "10px";
+	deleteButton.style.borderRadius = "5px";
 	deleteButton.style.border = "none";
 	deleteButton.addEventListener("click", () => deleteTaskElement(task.id))
 
@@ -44,6 +73,17 @@ function createTaskElement (task) {
 
 	li.appendChild(divTaskContent);
 	li.appendChild(divTaskAction);
+
+	const updateButton = document.createElement("button")
+	updateButton.textContent = "Actualizar"
+
+	updateButton.style.background = "linear-gradient(135deg,#3b82f6,#06b6d4)";
+	updateButton.style.border = "none";
+	updateButton.style.borderRadius = "5px"
+
+	updateButton.addEventListener("click", () => loadTaskToForm(task));
+
+	divTaskAction.appendChild(updateButton);
 
 // 	const liTemplate = `
 // <li id="${task.id}" class="task-item">
@@ -61,7 +101,43 @@ function createTaskElement (task) {
 	return li;
 }
 
+function loadTaskToForm(task){
+	document.getElementById("NuevaTarea").textContent = "Actualizar Tarea";
+   
+	document.querySelector("#agregar button").innerHTML ='<i class="fa-solid fa-pen"></i> Actualizar';
+
+	document.getElementById("cancelar").style.display = "block";
+	
+	document.querySelector('input[name="title"]').value = task.title;
+
+	document.querySelector('textarea[name="description"]').value = task.description;
+
+	editingTaskId = task.id;
+}
+
 function deleteTaskElement(taskId) {
 	const li = document.getElementById(taskId);
 	li.remove();
 }
+
+document.getElementById("cancelar")
+.addEventListener("click", () => {
+
+	document.querySelector('input[name="title"]').value = "";
+
+	document.querySelector('textarea[name="description"]').value = "";
+
+	editingTaskId = null;
+
+	alert("Se ha cancelado la acción");
+
+	document.getElementById("NuevaTarea").textContent =
+	"Nueva Tarea";
+
+	document.querySelector("#agregar button").innerHTML =
+	'<i class="fa-solid fa-plus"></i> Agregar Tarea';
+
+	document.getElementById("cancelar").style.display =
+	"none";
+});
+
